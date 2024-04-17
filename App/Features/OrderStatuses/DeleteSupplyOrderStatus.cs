dca92f054a2daa4616c6d429d182853374b1b90d
DunjaZamaklar <56863991+DunjaZamaklar@users.noreply.github.com>
2024-04-17 14:17:26 +0200

    Delete/Get all implemented

---
+++ App/Features/OrderStatuses/DeleteSupplyOrderStatus.cs
@@ -0,0 +1,60 @@
+﻿using App.Contracts;
+using App.Data.Database;
+using Carter;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using static App.Features.OrderStatuses.DeleteSupplyOrderStatus;
+
+namespace App.Features.OrderStatuses;
+
+public static class DeleteSupplyOrderStatus
+{
+
+    public class Query : IRequest<Boolean>
+    {
+        public Guid Id { get; set; }
+    }
+
+    internal sealed class Handler : IRequestHandler<Query, Boolean>
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public Handler(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<Boolean> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var entityToDelete = await _applicationDbContext.SupplyOrderStatus.FindAsync(request.Id);
+            if (entityToDelete != null)
+            {
+                _applicationDbContext.SupplyOrderStatus.Remove(entityToDelete);
+                // Save changes to the database
+                await _applicationDbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
+public class DeleteSupplyOrderStatusEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("supplyOrderStatus/{id}", async (Guid id, ISender sender) =>
+        {
+            var query = new DeleteSupplyOrderStatus.Query { Id = id };
+            var result = await sender.Send(query).ConfigureAwait(false);
+            if (result)
+            {
+                return Results.Ok();
+            }
+            else
+            {
+                return Results.NotFound();
+            }
+        });
+    }
+}
---
+++ App/Features/OrderStatuses/GetSupplyOrderStatuses.cs
@@ -0,0 +1,57 @@
+﻿using App.Contracts;
+using App.Data.Database;
+using Carter;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using static App.Features.OrderStatuses.CreateSupplyOrderStatus;
+
+namespace App.Features.OrderStatuses;
+
+public static class GetSupplyOrderStatuses
+{
+    public class Query : IRequest<List<SupplyOrderStatusResponse>>
+    {
+    }
+
+    internal sealed class Handler : IRequestHandler<Query, List<SupplyOrderStatusResponse>>
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public Handler(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<List<SupplyOrderStatusResponse>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var supplyOrderStatusResponse = await _applicationDbContext.SupplyOrderStatus
+                .Select(p => new SupplyOrderStatusResponse
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Description = p.Description
+                })
+                .ToListAsync(cancellationToken)
+                .ConfigureAwait(false);
+
+            return supplyOrderStatusResponse;
+        }
+    }
+}
+
+public class GetAllSupplyOrderStatusEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("supplyOrderStatus", async (ISender sender) =>
+        {
+            var query = new GetSupplyOrderStatuses.Query { };
+            var result = await sender.Send(query).ConfigureAwait(false);
+            if (result is null)
+            {
+                return null;
+            }
+            return result;
+        });
+    }
+}
